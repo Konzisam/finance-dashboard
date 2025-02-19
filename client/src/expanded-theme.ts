3f0e6e61fe81f306f0006c4030a94de6fcd3c3f1
Konzisam <konzisamuel@gmail.com>
2025-02-19 23:22:03 +0100

    fix type augmentation

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,4 +1,6 @@
 // eslint-disable-next-line
+// @ts-ignore
+import { Palette, PaletteColor } from "@mui/material/styles";
 
 declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
@@ -7,5 +9,9 @@ declare module "@mui/material/styles/createPalette" {
 
   interface Palette {
     tertiary: PaletteColor;
+  }
+
+  interface PaletteOptions {
+    tertiary?: PaletteColor;
   }
 }
